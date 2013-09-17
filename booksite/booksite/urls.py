623b2e05df278f0274f88a2914b1aac90e4cca4d
t.waner <t.waner@gmail.com>
2013-09-16 22:39:15 -0400

    Mid chapter 8

--- booksite/booksite/urls.py
+++ booksite/booksite/urls.py
@@ -1,25 +1,28 @@
-from django.conf.urls import patterns, include, url
-from booksite.views import hello, current_datetime, hours_ahead,display_meta
+from django.conf.urls.defaults import *
 from django.contrib import admin
-from books import views as bviews
-from contact import views as cviews
 admin.autodiscover()
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-	url(r'^hello/$', hello),
-	url(r'^time/$', current_datetime),
-	url(r'^another-time-page/$', current_datetime),
-	url(r'^time/plus/(\d{1,2})/$', hours_ahead),
+urlpatterns = patterns('booksite.views',
+	url(r'^hello/$', 'hello'),
+	url(r'^time/$', 'current_datetime'),
+	url(r'^another-time-page/$', 'current_datetime'),
+	url(r'^time/plus/(\d{1,2})/$', 'hours_ahead'),
+	url(r'^hours/plus/(?P<offset>\d{1,2})/$', 'hours_ahead'),
 	url(r'^admin/', include(admin.site.urls)),
-	url(r'^display-meta/',display_meta), 
-	url(r'^search-form/$', bviews.search_form),
-	url(r'^search/$', bviews.search),
-	url(r'^contact-form/$', cviews.contact),
-	
+	url(r'^display-meta/','display_meta'),
+)
+urlpatterns += patterns('books.views', 
+	url(r'^search-form/$', 'search_form'),
+	url(r'^search/$', 'search'),
+)
+urlpatterns += patterns('contact.views',
+	url(r'^contact-form/$', 'contact'),
+)
+		
     # Examples:
     # url(r'^$', 'booksite.views.home', name='home'),
     # url(r'^booksite/', include('booksite.foo.urls')),
@@ -29,4 +32,4 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-)
+
